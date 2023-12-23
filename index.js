1d652935401dc88b87b4f2b964fbc9d1b995647a
kamrul-7 <kamrul7.khan@gmail.com>
2023-12-24 03:36:49 +0600

    final

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zwakwnm.mongodb.net/?retryWrites=true&w=majority`;
 
+// const uri=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvo2oe4.mongodb.net/?retryWrites=true&w=majority`;
+
+
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
